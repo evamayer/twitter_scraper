84d2c96d4302ebfb893832aa687ec83120a44d92
Eva Mayer <eva.mayer@hyperisland.se>
2022-12-30 15:36:39 +0100

    improved word cloud

--- reader.py
+++ reader.py
@@ -1,9 +1,11 @@
 import re
+import numpy
+from PIL import Image
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 def filterFunc(x):
-    if "#" in x or "@" in x or re.match("a|the|an|the|to|in|for|of|or|by|with|is|on|that|be|from|https|\\n|//t|me|we|you|my|this|it", x):
+    if "#" in x or "@" in x or re.match("a|the|an|the|to|in|for|of|or|by|with|is|on|that|be|from|https|\\n|//t|me|we|you|my|this|it|s|I|&amp|not|The|was|what|what|but", x):
         return False
     else:
         return True
@@ -19,14 +21,16 @@ word_frequencies = {}
 
 for line in linelist:
     for word in split_words(line):
-        if word in word_frequencies:
-            word_frequencies[word] += 1
+        word_lower = word.lower()
+        if word_lower in word_frequencies:
+            word_frequencies[word_lower] += 1
         else:
-            word_frequencies[word] = 1
+            word_frequencies[word_lower] = 1
 
 # print(word_frequencies)
 
-wc = WordCloud(background_color="white", min_word_length=3, width=1600, height=900)
+mask = numpy.array(Image.open("runner.jpg"))
+wc = WordCloud(background_color="white", min_word_length=3, width=1600, height=900, max_words = 120, mask=mask)
 wc.generate_from_frequencies(word_frequencies)
 
 plt.axis("off")
