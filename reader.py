dc7c9653fbeba119998e4997e55756a7d21be8bb
Eva Mayer <eva.mayer@hyperisland.se>
2022-12-30 16:00:24 +0100

    updated code and images

--- reader.py
+++ reader.py
@@ -4,6 +4,7 @@ from PIL import Image
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+
 def filterFunc(x):
     if "#" in x or "@" in x or re.match("a|the|an|the|to|in|for|of|or|by|with|is|on|that|be|from|https|\\n|//t|me|we|you|my|this|it|s|I|&amp|not|The|was|what|what|but", x):
         return False
@@ -14,26 +15,28 @@ def split_words(text):
     result = re.split("\.|\?|\,|\-|\'|\"|\!|\:|\;|\ ", text)
     return list(filter(filterFunc, result))
 
-f = open("running.txt", encoding="ascii", errors='ignore')
-linelist = f.readlines()
+def visualize_words(textfile, imagefile):
 
-word_frequencies = {}
+    f = open(textfile, encoding="ascii", errors='ignore')
+    linelist = f.readlines()
 
-for line in linelist:
-    for word in split_words(line):
-        word_lower = word.lower()
-        if word_lower in word_frequencies:
-            word_frequencies[word_lower] += 1
-        else:
-            word_frequencies[word_lower] = 1
+    word_frequencies = {}
 
-# print(word_frequencies)
+    for line in linelist:
+        for word in split_words(line):
+            word_lower = word.lower()
+            if word_lower in word_frequencies:
+                word_frequencies[word_lower] += 1
+            else:
+                word_frequencies[word_lower] = 1
 
-mask = numpy.array(Image.open("runner.jpg"))
-wc = WordCloud(background_color="white", min_word_length=3, width=1600, height=900, max_words = 120, mask=mask)
-wc.generate_from_frequencies(word_frequencies)
+    mask = numpy.array(Image.open(imagefile))
+    wc = WordCloud(background_color="white", min_word_length=3, width=1600, height=900, max_words = 120, mask=mask)
+    wc.generate_from_frequencies(word_frequencies)
 
-plt.axis("off")
-plt.imshow(wc, interpolation="bilinear")
-plt.show()
+    plt.axis("off")
+    plt.imshow(wc, interpolation="bilinear")
+    plt.show()
     
+
+visualize_words("running.txt", "runner.jpg")
